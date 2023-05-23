881c969dd298c15cac73aec50452a66529aea457
Eap2468 <68890963+Eap2468@users.noreply.github.com>
2023-05-23 13:38:30 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,57 @@
+//
+//  main.cpp
+//  File transpher
+//
+//  Created by Ethan Phelps on 5/23/23.
+//
+
+#include <iostream>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fstream>
+
+#define BUFFERSIZE 1024
+
+int main(int argc, const char * argv[]) {
+    const char* filePath = "";
+    const char* ip = "";
+    
+    int serverfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(ip);
+    addr.sin_port = htons(4444);
+    socklen_t addrSize = sizeof(addr);
+    
+    int enable = 1;
+    setsockopt(serverfd, SOL_SOCKET, SO_REUSEPORT, &enable, sizeof(int));
+    
+    bind(serverfd, (sockaddr*)&addr, addrSize);
+    listen(serverfd, 1);
+    
+    std::cout << "server started" << std::endl;
+    
+    int clientfd = accept(serverfd, (sockaddr*)&addr, &addrSize);
+    
+    std::ifstream upload(filePath, std::ios::binary);
+    
+    upload.seekg(0, upload.end);
+    long bytes = upload.tellg();
+    upload.seekg(0, upload.beg);
+    
+    char buffer[BUFFERSIZE];
+    int bytes_read = 0;
+    
+    while((bytes_read = upload.read(buffer, BUFFERSIZE).gcount()) > 0)
+    {
+        if(send(clientfd, buffer, bytes_read, 0) != bytes_read)
+        {
+            std::cout << "Error" << std::endl;
+            return -1;
+        }
+    }
+    upload.close();
+    close(clientfd);
+    close(serverfd);
+    return 0;
+}
